fdb7d795fdbbe601e2d5dff97eff8c29fdaf6a3c
Vardhan reddy <vardhanreddy@Vardhans-MacBook-Air.local>
2026-04-24 02:12:54 +0530

    SEO: Set default title to 'Quantum Heights' for all pages

---
+++ src/components/SeoHead.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function SeoHead() {
+  return (
+    <Head>
+      <title>Quantum Heights</title>
+      <meta name="title" content="Quantum Heights" />
+    </Head>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import SeoHead from "@/components/SeoHead";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SeoHead />
+      <Component {...pageProps} />
+    </>
+  );
 }
